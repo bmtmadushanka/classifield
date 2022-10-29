2ce96ffdb132e7a9b07afdea5c9b97cb8178ea7a
Kusal Ranasinghe <62254709+kusal1998@users.noreply.github.com>
2022-10-29 23:14:55 +0530

    new changes

---
+++ resources/views/frontend/_partials/header.blade.php
@@ -0,0 +1,203 @@
+ <!-- START SECTION HEADINGS -->
+        <!-- Header Container
+        ================================================== -->
+        <header id="header-container">
+            <!-- Header -->
+            <div id="header">
+                <div class="container container-header">
+                    <!-- Left Side Content -->
+                    <div class="left-side">
+                        <!-- Logo -->
+                        <div id="logo">
+                            <a href="index.html"><img src="{{asset('frontend/assets/images/logo.svg')}}')}}" alt=""></a>
+                        </div>
+                        <!-- Mobile Navigation -->
+                        <div class="mmenu-trigger">
+                            <button class="hamburger hamburger--collapse" type="button">
+                                <span class="hamburger-box">
+							<span class="hamburger-inner"></span>
+                                </span>
+                            </button>
+                        </div>
+                        <!-- Main Navigation -->
+                        <nav id="navigation" class="style-1">
+                            <ul id="responsive">
+                                <li><a href="#">Home</a>
+                                    <ul>
+                                        <li><a href="index.html">Home Image</a></li>
+                                        <li><a href="index-2.html">Home Image-Video</a></li>
+                                        <li><a href="index-3.html">Parallax Style 1</a></li>
+                                        <li><a href="index-4.html">Map Style 1</a></li>
+                                        <li><a href="index-5.html">Home Video</a></li>
+                                        <li><a href="index-6.html">Home Slider</a></li>
+                                        <li><a href="index-7.html">Parallax Style 2</a></li>
+                                        <li><a href="index-8.html">Map Style 2</a></li>
+                                        <li><a href="index-9.html">Fixed Image</a></li>
+                                        <li><a href="index-10.html">Search Form</a></li>
+                                        <li><a href="index-11.html">Slider Presentation</a></li>
+                                        <li><a href="index-12.html">Food and Restaurant</a></li>
+                                        <li><a href="index-13.html">Airbnb and Real Estate</a></li>
+                                        <li><a href="index-14.html">Hotel and Resort</a></li>
+                                        <li><a href="index-15.html">Car Sale and Rent</a></li>
+                                        <li><a href="index-16.html">Job Board</a></li>
+                                    </ul>
+                                </li>
+                                <li><a href="#">Listings</a>
+                                    <ul>
+                                        <li><a href="#">List Layout</a>
+                                            <ul>
+                                                <li><a href="listings-full-list.html">Full Width</a></li>
+                                                <li><a href="listings-list-right-sidebar.html">With Sidebar</a></li>
+                                            </ul>
+                                        </li>
+                                        <li><a href="#">Grid Layout</a>
+                                            <ul>
+                                                <li><a href="listings-full-grid.html">Full Width</a></li>
+                                                <li><a href="listings-grid-right-sidebar.html">With Sidebar</a></li>
+                                                <li><a href="listings-half-map.html">Half Map</a></li>
+                                                <li><a href="listings-map.html">Top Map</a></li>
+                                            </ul>
+                                        </li>
+                                        <li><a href="#">Single Listings</a>
+                                            <ul>
+                                                <li><a href="listing-details.html">Single Listing 1</a></li>
+                                                <li><a href="listing-details-two.html">Single Listing 2</a></li>
+                                                <li><a href="listing-details-three.html">Single Listing 3</a></li>
+                                                <li><a href="listing-details-four.html">Single Listing 4</a></li>
+                                                <li><a href="listing-details-five.html">Single Listing 5</a></li>
+                                                <li><a href="listing-details-six.html">Single Listing 6</a></li>
+                                            </ul>
+                                        </li>
+                                    </ul>
+                                </li>
+                                <li><a href="#">Pages</a>
+                                    <ul>
+                                        <li><a href="#">Shop</a>
+                                            <ul>
+                                                <li><a href="shop-with-sidebar.html">Product Sidebar</a></li>
+                                                <li><a href="shop-full-page.html">Product Fullpage</a></li>
+                                                <li><a href="shop-single.html">Product Single</a></li>
+                                                <li><a href="shop-checkout.html">Checkout Page</a></li>
+                                                <li><a href="shop-order.html">Order Page</a></li>
+                                            </ul>
+                                        </li>
+                                        <li><a href="#">User Panel</a>
+                                            <ul>
+                                                <li><a href="dashboard.html">Dashboard</a></li>
+                                                <li><a href="user-profile.html">User Profile</a></li>
+                                                <li><a href="my-listings.html">My Listings</a></li>
+                                                <li><a href="favorited-listings.html">Favorited Listings</a></li>
+                                                <li><a href="add-listing.html">Add Listing</a></li>
+                                                <li><a href="payment-method.html">Payment Method</a></li>
+                                                <li><a href="invoice.html">Invoice</a></li>
+                                                <li><a href="change-password.html">Change Password</a></li>
+                                            </ul>
+                                        </li>
+                                        <li><a href="about.html">About Us</a></li>
+                                        <li><a href="faq.html">Faq</a></li>
+                                        <li><a href="pricing-table.html">Pricing Tables</a></li>
+                                        <li><a href="payment-method.html">Payment Method</a></li>
+                                        <li><a href="invoice.html">Invoice</a></li>
+                                        <li><a href="thank-you.html">Thank You Page</a></li>
+                                        <li><a href="404.html">Page 404</a></li>
+                                        <li><a href="login.html">Login</a></li>
+                                        <li><a href="register.html">Register</a></li>
+                                        <li><a href="coming-soon.html">Coming Soon</a></li>
+                                        <li><a href="under-construction.html">Under Construction</a></li>
+                                        <li><a href="ui-element.html">UI Elements</a></li>
+                                    </ul>
+                                </li>
+                                <li><a href="#">Blog</a>
+                                    <ul>
+                                        <li><a href="#">Grid Layout</a>
+                                            <ul>
+                                                <li><a href="blog-grid.html">Full Grid</a></li>
+                                                <li><a href="blog-grid-sidebar.html">With Sidebar</a></li>
+                                            </ul>
+                                        </li>
+                                        <li><a href="#">List Layout</a>
+                                            <ul>
+                                                <li><a href="blog-list.html">Full List</a></li>
+                                                <li><a href="blog-list-sidebar.html">With Sidebar</a></li>
+                                            </ul>
+                                        </li>
+                                        <li><a href="blog-details.html">Blog Details</a></li>
+                                    </ul>
+                                </li>
+                                <li><a href="contact-us.html">Contact</a></li>
+                                <li class="d-none d-xl-none d-block d-lg-block"><a href="login.html">Login</a></li>
+                                <li class="d-none d-xl-none d-block d-lg-block"><a href="register.html">Register</a></li>
+                                <li class="d-none d-xl-none d-block d-lg-block mt-5 pb-4 ml-5 border-bottom-0"><a href="add-listing.html" class="button border btn-lg btn-block text-center">Add Listing<i class="fas fa-laptop-house ml-2"></i></a></li>
+                            </ul>
+                        </nav>
+                        <!-- Main Navigation / End -->
+                    </div>
+                    <!-- Left Side Content / End -->
+
+                    <!-- Right Side Content / End -->
+                    <div class="right-side d-none d-none d-lg-none">
+                        <!-- Header Widget -->
+                        <div class="header-widget">
+                            <a href="add-listing.html" class="button border">Add Listing<i class="fas fa-laptop-house ml-2"></i></a>
+                        </div>
+                        <!-- Header Widget / End -->
+                    </div>
+                    <!-- Right Side Content / End -->
+
+                    <!-- Right Side Content / End -->
+                    <div class="header-user-menu user-menu add d-none">
+                        <div class="header-user-name">
+                            <span><img src="{{asset('frontend/assets/images/testimonials/ts-1.jpg')}}" alt=""></span>Hi, Mary!
+                        </div>
+                        <ul>
+                            <li><a href="user-profile.html">Edit profile</a></li>
+                            <li><a href="add-listing.html">Add Listing</a></li>
+                            <li><a href="payment-method.html">Payments</a></li>
+                            <li><a href="change-password.html">Change Password</a></li>
+                            <li><a href="#">Log Out</a></li>
+                        </ul>
+                    </div>
+                    <!-- Right Side Content / End -->
+
+                    <div class="right-side d-none d-none d-lg-none d-xl-flex sign ml-0">
+                        <!-- Header Widget -->
+                      
+                        @guest
+                            @if (Route::has('login'))
+                            <div class="header-widget sign-in">
+                                <a  href="{{ route('login') }}">Sign In</a>
+                            </div>
+                            @endif
+                        @else
+                        @if (Route::has('register'))
+                        <div class="header-widget sign-in">
+                            <a href="{{ route('register') }}">Register</a>
+                        </div>
+                        @endif
+                        @endguest
+                       
+                        <!-- Header Widget / End -->
+                    </div>
+                    <!-- Right Side Content / End -->
+
+                    <!-- lang-wrap-->
+                    <div class="header-user-menu user-menu add d-none d-lg-none d-xl-flex">
+                        <div class="lang-wrap">
+                            <div class="show-lang"><span><i class="fas fa-globe-americas"></i><strong>ENG</strong></span><i class="fa fa-caret-down arrlan"></i></div>
+                            <ul class="lang-tooltip lang-action no-list-style">
+                                <li><a href="#" class="current-lan" data-lantext="En">English</a></li>
+                                <li><a href="#" data-lantext="Fr">Francais</a></li>
+                                <li><a href="#" data-lantext="Es">Espanol</a></li>
+                                <li><a href="#" data-lantext="De">Deutsch</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <!-- lang-wrap end-->
+
+                </div>
+            </div>
+            <!-- Header / End -->
+
+        </header>
+        <div class="clearfix"></div>
+        <!-- Header Container / End -->
