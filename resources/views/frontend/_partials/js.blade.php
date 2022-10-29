5edfc97a825b8c838e3432ba6b2e36701e0f089f
Kusal Ranasinghe <62254709+kusal1998@users.noreply.github.com>
2022-10-29 15:49:08 +0530

    js

---
+++ resources/views/frontend/_partials/js.blade.php
@@ -0,0 +1,59 @@
+<!-- ARCHIVES JS -->
+<script src="{{asset('frontend/assets/js/jquery-3.5.1.min.js')}}"></script>
+<script src="{{asset('frontend/assets/js/rangeSlider.js')}}"></script>
+<script src="{{asset('frontend/assets/js/rangeSlider.js')}}"></script>
+<script src="{{asset('frontend/assets/js/tether.min.js')}}"></script>
+<script src="{{asset('frontend/assets/js/moment.js')}}"></script>
+<script src="{{asset('frontend/assets/js/bootstrap.min.js')}}"></script>
+<script src="{{asset('frontend/assets/js/mmenu.min.js')}}"></script>
+<script src="{{asset('frontend/assets/js/mmenu.js')}}"></script>
+<script src="{{asset('frontend/assets/js/aos.js')}}"></script>
+<script src="{{asset('frontend/assets/js/aos2.js')}}"></script>
+<script src="{{asset('frontend/assets/js/aos2.js')}}"></script>
+<script src="{{asset('frontend/assets/js/fitvids.js')}}"></script>
+<script src="{{asset('frontend/assets/js/jquery.waypoints.min.js')}}"></script>
+<script src="{{asset('frontend/assets/js/jquery.counterup.min.js')}}"></script>
+<script src="{{asset('frontend/assets/js/imagesloaded.pkgd.min.js')}}"></script>
+<script src="{{asset('frontend/assets/js/isotope.pkgd.min.js')}}"></script>
+<script src="{{asset('frontend/assets/js/smooth-scroll.min.js')}}"></script>
+<script src="{{asset('frontend/assets/js/lightcase.js')}}"></script>
+<script src="{{asset('frontend/assets/js/search.js')}}"></script>
+<script src="{{asset('frontend/assets/js/slick.min.js')}}"></script>
+<script src="{{asset('frontend/assets/js/slick-home-1.js')}}"></script>
+<script src="{{asset('frontend/assets/js/owl.carousel.js')}}"></script>
+<script src="{{asset('frontend/assets/js/jquery.magnific-popup.min.js')}}"></script>
+<script src="{{asset('frontend/assets/js/ajaxchimp.min.js')}}"></script>
+<script src="{{asset('frontend/assets/js/newsletter.js')}}"></script>
+<script src="{{asset('frontend/assets/js/jquery.form.js')}}"></script>
+<script src="{{asset('frontend/assets/js/jquery.validate.min.js')}}"></script>
+<script src="{{asset('frontend/assets/js/searched.js')}}"></script>
+<script src="{{asset('frontend/assets/js/forms-2.js')}}"></script>
+<script src="{{asset('frontend/assets/js/leaflet.js')}}"></script>
+<script src="{{asset('frontend/assets/js/leaflet.js')}}"></script>
+<script src="{{asset('frontend/assets/js/leaflet-gesture-handling.min.js')}}"></script>
+<script src="{{asset('frontend/assets/js/map4.js')}}"></script>
+<script src="{{asset('frontend/assets/js/leaflet-providers.js')}}"></script>
+<script src="{{asset('frontend/assets/js/range.js')}}"></script>
+<script src="{{asset('frontend/assets/js/color-switcher.js')}}"></script>
+
+<script src="{{asset('frontend/assets/revolution/js/jquery.themepunch.tools.min.js')}}"></script>
+<script src="{{asset('frontend/assets/revolution/js/jquery.themepunch.revolution.min.js')}}"></script>
+<script src="{{asset('frontend/assets/revolution/js/extensions/revolution.extension.actions.min.js')}}"></script>
+<script src="{{asset('frontend/assets/revolution/js/extensions/revolution.extension.carousel.min.js')}}"></script>
+<script src="{{asset('frontend/assets/revolution/js/extensions/revolution.extension.kenburn.min.js')}}"></script>
+<script src="{{asset('frontend/assets/revolution/js/extensions/revolution.extension.layeranimation.min.js')}}"></script>
+<script src="{{asset('frontend/assets/revolution/js/extensions/revolution.extension.migration.min.js')}}"></script>
+<script src="{{asset('frontend/assets/revolution/js/extensions/revolution.extension.navigation.min.js')}}"></script>
+<script src="{{asset('frontend/assets/revolution/js/extensions/revolution.extension.parallax.min.js')}}"></script>
+<script src="{{asset('frontend/assets/revolution/js/extensions/revolution.extension.slideanims.min.js')}}"></script>
+<script src="{{asset('frontend/assets/revolution/js/extensions/revolution.extension.video.min.js')}}"></script>
+<script src="{{asset('frontend/assets/js/script.js')}}"></script>
+
+<script>
+    $(".dropdown-filter").on('click', function() {
+
+        $(".explore__form-checkbox-list").toggleClass("filter-block");
+
+    });
+
+</script>
