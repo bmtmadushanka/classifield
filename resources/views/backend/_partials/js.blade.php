24636b43e8864cfc2f2437bebd4c937a3212d97e
Kusal Ranasinghe <62254709+kusal1998@users.noreply.github.com>
2022-10-29 22:33:19 +0530

    admin bar

---
+++ resources/views/backend/_partials/js.blade.php
@@ -0,0 +1,2 @@
+<script src="{{asset('frontend/assets/scripts/main.js')}}"></script>
+<script src="http://maps.google.com/maps/api/js?sensor=true"></script>
---
+++ resources/views/backend/_partials/style.blade.php
@@ -0,0 +1,1 @@
+<link rel="stylesheet" href="{{asset('backend/assets/main.css')}}">
