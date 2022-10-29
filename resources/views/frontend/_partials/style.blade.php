552aaff70c93ab84b85f2248ad04404709f1c6e6
Kusal Ranasinghe <62254709+kusal1998@users.noreply.github.com>
2022-10-29 14:48:03 +0530

    new layouts

---
+++ resources/views/frontend/_partials/style.blade.php
@@ -0,0 +1,31 @@
+<!-- Font Awesome -->
+<link rel="stylesheet" href="{{asset('frontend/assets/css/fontawesome-all.min.css')}}">
+<link rel="stylesheet" href="{{asset('frontend/assets/css/fontawesome-5-all.min.css')}}">
+<link rel="stylesheet" href="{{asset('frontend/assets/css/font-awesome.min.css')}}">
+<link rel="stylesheet" href="{{asset('frontend/assets/css/leaflet.css')}}">
+
+<link rel="stylesheet" href="{{asset('frontend/assets/css/leaflet.css')}}">
+<link rel="stylesheet" href="{{asset('frontend/assets/css/leaflet-gesture-handling.min.css')}}">
+<link rel="stylesheet" href="{{asset('frontend/assets/css/leaflet.markercluster.css')}}">
+<link rel="stylesheet" href="{{asset('frontend/assets/css/leaflet.markercluster.default.css')}}">
+
+<link rel="stylesheet" href="{{asset('frontend/assets/revolution/css/settings.css')}}">
+<link rel="stylesheet" href="{{asset('frontend/assets/revolution/css/layers.css')}}">
+<link rel="stylesheet" href="{{asset('frontend/assets/revolution/css/navigation.css')}}">
+
+<link rel="stylesheet" href="{{asset('frontend/assets/css/search-form.css')}}">
+<link rel="stylesheet" href="{{asset('frontend/assets/css/search.css')}}">
+<link rel="stylesheet" href="{{asset('frontend/assets/css/animate.css')}}">
+<link rel="stylesheet" href="{{asset('frontend/assets/css/aos.css')}}">
+<link rel="stylesheet" href="{{asset('frontend/assets/css/aos2.css')}}">
+<link rel="stylesheet" href="{{asset('frontend/assets/css/magnific-popup.css')}}">
+<link rel="stylesheet" href="{{asset('frontend/assets/css/lightcase.css')}}">
+<link rel="stylesheet" href="{{asset('frontend/assets/css/owl-carousel.css')}}">
+<link rel="stylesheet" href="{{asset('frontend/assets/css/bootstrap.min.css')}}">
+<link rel="stylesheet" href="{{asset('frontend/assets/css/menu.css')}}">
+<link rel="stylesheet" href="{{asset('frontend/assets/css/slick.css')}}">
+<link rel="stylesheet" href="{{asset('frontend/assets/css/styles.css')}}">
+<link rel="stylesheet" href="{{asset('frontend/assets/css/maps.css')}}">
+
+
+ @yield('styles')
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\FrontendController;
+
 
 
 /*
@@ -16,13 +18,17 @@ use App\Http\Controllers\HomeController;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-  
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', [FrontendController::class, 'index'])->name('index');
+
 Auth::routes();
   
 Route::get('/home', [HomeController::class, 'index'])->name('home');
+
+
   
 Route::group(['middleware' => ['auth']], function() {
     Route::resource('roles', RoleController::class);
