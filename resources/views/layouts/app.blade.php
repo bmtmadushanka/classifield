6e00dabd19c030d082d6d2ee86bad66ed54b6697
Kusal Ranasinghe <62254709+kusal1998@users.noreply.github.com>
2022-10-29 13:09:01 +0530

    styles

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,61 +11,17 @@
     <!-- Fonts -->
     <link rel="dns-prefetch" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css">
+    <link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
+    <link rel="stylesheet" href="css/jquery-ui.css">
+    <!-- GOOGLE FONTS -->
+    <link href="https://fonts.googleapis.com/css?family=Asap:400,400i%7CMontserrat:600,700,800" rel="stylesheet">
+    <link rel="stylesheet" id="color" href="css/default.css">
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    Laravel 8 User Roles and Permissions - ItSolutionStuff.com
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-    
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto"></ul>
-
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li><a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a></li>
-                            <li><a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a></li>
-                        @else
-                            <li><a class="nav-link" href="{{ route('users.index') }}">Manage Users</a></li>
-                            <li><a class="nav-link" href="{{ route('roles.index') }}">Manage Role</a></li>
-                            <li><a class="nav-link" href="{{ route('products.index') }}">Manage Product</a></li>
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
-
-
-                                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
-            </div>
-        </nav>
-
-
+        @include('layouts.header')
         <main class="py-4">
             <div class="container">
             @yield('content')
---
+++ resources/views/layouts/style.blade.php
@@ -0,0 +1,31 @@
+<!-- Font Awesome -->
+<link rel="stylesheet" href="{{asset('assets/css/fontawesome-all.min.css')}}">
+<link rel="stylesheet" href="{{asset('assets/css/fontawesome-5-all.min.css')}}">
+<link rel="stylesheet" href="{{asset('assets/css/font-awesome.min.css')}}">
+<link rel="stylesheet" href="{{asset('assets/css/leaflet.css')}}">
+
+<link rel="stylesheet" href="{{asset('assets/css/leaflet.css')}}">
+<link rel="stylesheet" href="{{asset('assets/css/leaflet-gesture-handling.min.css')}}">
+<link rel="stylesheet" href="{{asset('assets/css/leaflet.markercluster.css')}}">
+<link rel="stylesheet" href="{{asset('assets/css/leaflet.markercluster.default.css')}}">
+
+<link rel="stylesheet" href="{{asset('assets/revolution/css/settings.css')}}">
+<link rel="stylesheet" href="{{asset('assets/revolution/css/layers.css')}}">
+<link rel="stylesheet" href="{{asset('assets/revolution/css/navigation.css')}}">
+
+<link rel="stylesheet" href="{{asset('assets/css/search-form.css')}}">
+<link rel="stylesheet" href="{{asset('assets/css/search.css')}}">
+<link rel="stylesheet" href="{{asset('assets/css/animate.css')}}">
+<link rel="stylesheet" href="{{asset('assets/css/aos.css')}}">
+<link rel="stylesheet" href="{{asset('assets/css/aos2.css')}}">
+<link rel="stylesheet" href="{{asset('assets/css/magnific-popup.css')}}">
+<link rel="stylesheet" href="{{asset('assets/css/lightcase.css')}}">
+<link rel="stylesheet" href="{{asset('assets/css/owl-carousel.css')}}">
+<link rel="stylesheet" href="{{asset('assets/css/bootstrap.min.css')}}">
+<link rel="stylesheet" href="{{asset('assets/css/menu.css')}}">
+<link rel="stylesheet" href="{{asset('assets/css/slick.css')}}">
+<link rel="stylesheet" href="{{asset('assets/css/styles.css')}}">
+<link rel="stylesheet" href="{{asset('assets/css/maps.css')}}">
+
+
+ @yield('styles')
